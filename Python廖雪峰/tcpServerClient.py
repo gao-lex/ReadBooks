265a5f77dad683f30278c14a9d755a3d3bd6251b
yacogo <1105856206@qq.com>
2017-03-11 17:04:52 +0800

    python-tcp-udp

---
+++ Python廖雪峰/tcpServerClient.py
@@ -0,0 +1,14 @@
+import socket
+# AF_INET指定使用IPv4协议，如果要用更先进的IPv6，就指定为AF_INET6
+# SOCK_STREAM指定使用面向流的TCP协议
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('127.0.0.1', 9999))
+# 接收欢迎消息:
+print(s.recv(1024).decode('utf-8'))
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
---
+++ Python廖雪峰/udp_server.py
@@ -0,0 +1,18 @@
+# 使用UDP协议时，不需要建立连接，只需要知道对方的IP地址和端口号，就可以直接发数据包。但是，能不能到达就不知道了。
+# -*- coding: utf-8 -*-
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# 绑定端口:
+s.bind(('127.0.0.1', 9999))
+
+print('Bind UDP on 9999...')
+
+while True:
+    # 接收数据:
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    reply = 'Hello, %s!' % data.decode('utf-8')
+    s.sendto(reply.encode('utf-8'), addr)
